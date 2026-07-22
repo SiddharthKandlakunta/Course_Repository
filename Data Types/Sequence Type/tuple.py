a2c00469677e9bb0fca01763932616bae4f4623c
SiddharthKandlakunta <kandlakuntasiddharth@gmail.com>
2026-07-22 11:23:54 +0530

    added datatypess and variables

---
+++ Data Types/Sequence Type/tuple.py
@@ -0,0 +1,11 @@
+##Lists and Tuples are quite similar but tuples are immutable, that means, you cannnot change its value after adding
+#tuple
+
+x = (1,2,3,'1',2)
+print(type(x))
+
+#Tuples are also indexed
+print(x[0])
+
+#Tuples cannot be changed so you cannot add or delete anything
+x.append(3)
---
+++ Data Types/numeric.py
@@ -0,0 +1,14 @@
+##Numeric##
+##int,float,complex
+print(type(12))
+print(type(-12))
+print(type(12+10.25))
+print(type(12+3j))
+x = None
+print(type(x))
+
+##Boolean
+print(type(True))
+print(type(False))
+print(type(1>5))
+print(1>5)
